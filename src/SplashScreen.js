395a4ebdb291c42e657ed9501c644e6028650e15
Johan Lim <jjohanlim@gmail.com>
2020-11-28 19:31:50 +0100

    play sound when a game starts

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import splash from './assets/titelskärm.png';
 import playbutton from './assets/spela.png';
+import { powerup } from './Audio';
 
 const socket = io('https://ollesspelserver.herokuapp.com/', { transports: ['websocket'], rejectUnauthorized: false });
 // const socket = io('http://localhost:3003', { transports: ['websocket'], rejectUnauthorized: false });
@@ -13,6 +14,7 @@ export default function SplashScreen(props) {
     useEffect(() => {
         socket.emit('checkForRooms');
         socket.on('roomsAvailable', (rooms) => {
+            if (rooms.length > 0) powerup.play();
             setAvailableRooms(rooms);
         })
     }, []);
@@ -27,12 +29,12 @@ export default function SplashScreen(props) {
                     <h2>Starta spelet</h2>
                     <p>Välj ett pågående multiplayer till höger, eller skriv in ditt namn och bara tryck på spela för att starta ett eget spel</p>
                     <input placeholder="Skriv ditt namn" className="nameentry" type="text" onChange={(e) => props.setPlayerName(e.target.value)} value={props.playerName} />
-                    <button className="startplaying" onClick={props.playGame}>Börja spela</button>
+                    <button disabled={!props.playerName} className="startplaying" onClick={props.playGame}>Börja spela</button>
                 </div>
                 <div>
                     <h2>Pågående spel</h2>
                     <div className="list">
-                        {availableRooms.sort((a, b) => b.lastUpdated - a.lastUpdated).map(room => <div className={props.multiPlayerRoom === room.number ? 'roomselected' : ''} key={room.number} onClick={() => props.updateMultiPlayerRoom(room.number)}>Spel startat av: <span className="startedby">{room.startedBy}</span> {room.players} inne <sup>({room.number})</sup></div>)}
+                        {availableRooms.sort((a, b) => b.lastUpdated - a.lastUpdated).map(room => <div className={props.multiPlayerRoom === room.number ? 'roomselected' : ''} key={room.number} onClick={() => props.updateMultiPlayerRoom(room.number)}><input type="checkbox" checked={props.multiPlayerRoom === room.number} />Spel startat av: <span className="startedby">{room.startedBy}</span> {room.players} inne <sup>({room.number})</sup></div>)}
                         {availableRooms.length === 0 && <span>(inga just nu...)</span>}
                     </div>
                 </div>
